9c1958876eb0d1225924730f41daa88fa5145e90
tomek <tomaszjanrys@gmail.com>
2022-04-22 16:45:41 +0200

    metoda

--- Program.cs
+++ Program.cs
@@ -1,4 +1,16 @@
-﻿int x = 1;
+﻿
+int x = 1;
 double x1 = (double) x;
 Console.WriteLine("Rzutowanie Int :"+x.GetType() + " na  Typ :"+x1.GetType());
 //this it  type casting
+
+static void metoda(string tekst="you did no provide argument")
+{
+    Console.WriteLine("Metoda :"+ tekst);
+}
+
+//metoda
+
+metoda();
+metoda(tekst:"I am Hero");
+//
