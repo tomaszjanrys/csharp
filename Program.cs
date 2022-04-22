78389444e53e16b17ec7afcdb27e7809186c518d
tomek <tomaszjanrys@gmail.com>
2022-04-22 17:29:57 +0200

    continue

--- Program.cs
+++ Program.cs
@@ -2,6 +2,11 @@
 int x = 1;
 double x1 = (double) x;
 Console.WriteLine("Rzutowanie Int :"+x.GetType() + " na  Typ :"+x1.GetType());
+
+int age = 33;
+string ageString = age.ToString();
+Console.WriteLine("Age to casting on string " + ageString + "Age type"+age.GetType()+"Age after casting" + ageString.GetType());
+
 //this it  type casting
 
 static void metoda(string tekst="you did no provide argument")
@@ -14,3 +19,20 @@ static void metoda(string tekst="you did no provide argument")
 metoda();
 metoda(tekst:"I am Hero");
 //
+//Type
+double doublenum = 2.2d;
+doublenum= 33.33;
+decimal decnum = 10.0m;
+decnum=33.66m;
+char charX= '2';
+Console.WriteLine("Double :" + doublenum + "Decimal num :"+ decnum+ "Char is :" + charX);
+string name , surname;
+name = "Bat";
+surname = "Man";
+Console.WriteLine($"Your name {name} and surname  {surname} ->"+ name+surname);
+
+bool IsWorking;
+
+IsWorking = false;
+
+Console.WriteLine("Bool "+ IsWorking);
