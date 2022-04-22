60668f55e6fc074a793a158741b2988a817ba3dd
tomek <tomaszjanrys@gmail.com>
2022-04-22 16:25:47 +0200

    //this it  type casting

--- Program.cs
+++ Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Test ");
+﻿int x = 1;
+double x1 = (double) x;
+Console.WriteLine("Rzutowanie Int :"+x.GetType() + " na  Typ :"+x1.GetType());
+//this it  type casting
