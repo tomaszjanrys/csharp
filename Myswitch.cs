15d269036ca1ea5505c32812b787cf3ecdedd9ef
pypyTom <ok>
2022-04-24 17:25:19 +0200

    switch

--- Myswitch.cs
+++ Myswitch.cs
@@ -5,6 +5,29 @@ namespace myswitchlekcja
         public static void mytest()
         {
             Console.WriteLine("Swicht");
+
+
+            DateTime data = new DateTime();
+
+            data = DateTime.Now;
+             
+            string mydata = string.Format("{0:dddd}",data);
+
+
+            switch(mydata)
+            {
+                case "niedziela":
+                    Console.WriteLine("Dzisiaj jest :" + mydata);
+                    break;
+                case "test":
+                    Console.WriteLine("no");
+                    break;
+                default:
+                    Console.WriteLine("Nie ma takiej opcji");
+                    break;
+
+            }
+
         }
     }
 }
